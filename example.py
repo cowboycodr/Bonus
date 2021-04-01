64df834ae71c6207cff10d77ba93b7cfa4d30b54
Kian <kian.p.m.007@gmail.com>
2021-03-31 19:10:08 -0500

    update example.py for better documentation

--- example.py
+++ example.py
@@ -1,10 +1,13 @@
 from src.core.app import App
 from src.core.window import Window
 
-class BonusWindow(Window):
+class ExampleWindow(Window):
     def __init__(self):
         super().__init__()
         
 if __name__ == '__main__':
-    app = App(BonusWindow())
+    app = App(ExampleWindow())
     app.run()
+    
+    # remove this code if you would like the application to save the size and location of the window
+    app.reset_window()

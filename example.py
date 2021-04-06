9f9b0afa84f1658ba99fa6057517d2fefa5f706b
Kian <kian.p.m.007@gmail.com>
2021-04-05 20:05:46 -0500

    update example.py

--- example.py
+++ example.py
@@ -7,18 +7,19 @@ class ExampleWindow(Window):
     def __init__(self):
         super().__init__()
         
-        # updates per second, default FPS is 60, do not rely on it being 100% accurate
+        # updates per second, default FPS is 60
         self.FPS = 60
         
         self.start = time.time()
-        
+    
     # gets called everytime the window updates
     def update(self, changes):
         self.title(self.avg_frame_rate())
     
 if __name__ == '__main__':
     app = App(ExampleWindow())
-    app.window.center()
+    app.window.resize(1920, 1080)
+    app.window.center(resize=True, size_hint_width=1.25, size_hint_height=1.7)
     app.run()
     
     appWindow = app.window
@@ -26,4 +27,4 @@ if __name__ == '__main__':
     print(app)
     
     # remove this code if you would like the application to save the size and location of the window when closed
-    app.reset_window()
+    # app.reset_window()
